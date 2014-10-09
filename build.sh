2810f37fe42044a456d82159d2386414f753fbef
geo2a <georgiylukjanov@gmail.com>
2014-10-09 15:07:00 +0400

    Now we can test connection with mobem ny sending it AT

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm dist/build/gsm-with-haskell/gsm-with-haskell
 cabal configure
 cabal build
--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,28 @@
 import System.IO
 import System.Hardware.Serialport
 import qualified Data.ByteString.Char8 as B 
+--import Data.String.Utils
 
 type Modem = SerialPort
 
 main :: IO ()
 main = do
   putStrLn "Hi, haskeller!"
-  withSerial "/dev/modem" defaultSerialSettings modemTest
+  testResult <- testConnect "/dev/modem"
+  print testResult
+  
 
-modemTest :: Modem -> IO()
-modemTest modem = do
-  send modem $ B.pack "AT\r"
-  recv modem 10 >>= print
+testConnect :: String -> IO Bool
+testConnect pathToModem = do
+  answ <- withSerial pathToModem defaultSerialSettings pingModem
+  return $ 
+    case (filter (/= '\r') . B.unpack $ answ) of
+      "0" -> True
+      "OK" -> True
+      _ -> False
+  where
+    pingModem :: Modem -> IO B.ByteString
+    pingModem modem = do
+      send modem $ B.pack "AT\r"
+      answ <- recv modem 10
+      return answ
