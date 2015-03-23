39e8c2cf068fc0282831180df4aaa7732367bd7c
Georgiy Lukjanov <georgiylukjanov@gmail.com>
2015-03-23 21:05:38 +0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -5,4 +5,4 @@ main = do
   putStrLn "Hi, haskeller!"
   testResult <- testConnect "/dev/modem"
   print testResult
-  sendSMS "+79085069512" "Hi, Bro" "/dev/modem"
+  sendSMS "phonenumber" "Hi, Bro" "/dev/modem"
